2381402cdf43308d5695ab7470cdff7516196b03
snaptics <operations@snaptics.in>
2025-02-14 14:52:09 +0530

    updated dashboard icon and image

--- src/app/components/aconsole/page.js
+++ src/app/components/aconsole/page.js
@@ -9,8 +9,8 @@ import "../../../../node_modules/ag-grid-community/styles/ag-theme-alpine.css";
 export default function Aconsle() {
   useEffect(() => {
     // Import Bootstrap JavaScript only on the client side
-    if (typeof window !== 'undefined') {
-      require('../../../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js');
+    if (typeof window !== "undefined") {
+      require("../../../../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js");
     }
     console.log("Component has mounted on the client side.");
   }, []);
@@ -35,55 +35,55 @@ export default function Aconsle() {
 
   useEffect(() => {
     fetchUserData();
-  }, []); 
+  }, []);
 
   const [columnDefs] = useState([
-    { headerName: "sector", field: "sector", sortable: true,  },
-    { headerName: "2011_12", field: "2011_12", sortable: true,  },
-    { headerName: "2012_13", field: "2012_13", sortable: true,  },
-    { headerName: "2013_14", field: "2013_14", sortable: true,  },
-    { headerName: "2014_15", field: "2014_15", sortable: true,  },
-    { headerName: "2015_16", field: "2015_16", sortable: true,  },
-    { headerName: "2016_17", field: "2016_17", sortable: true,  },
-    { headerName: "2017_18", field: "2017_18", sortable: true,  },
-    { headerName: "2018_19", field: "2018_19", sortable: true,  },
-    { headerName: "2019_20", field: "2019_20", sortable: true,  },
-    { headerName: "2020_21_TRE", field: "2020_21_TRE", sortable: true,  },
-    { headerName: "2021_22_SRE", field: "2021_22_SRE", sortable: true,  },
-    { headerName: "2022_23_FRE", field: "2022_23_FRE", sortable: true,  },
-    { headerName: "2023_24_PE", field: "2023_24_PE", sortable: true,  },
+    { headerName: "sector", field: "sector", sortable: true },
+    { headerName: "2011_12", field: "2011_12", sortable: true },
+    { headerName: "2012_13", field: "2012_13", sortable: true },
+    { headerName: "2013_14", field: "2013_14", sortable: true },
+    { headerName: "2014_15", field: "2014_15", sortable: true },
+    { headerName: "2015_16", field: "2015_16", sortable: true },
+    { headerName: "2016_17", field: "2016_17", sortable: true },
+    { headerName: "2017_18", field: "2017_18", sortable: true },
+    { headerName: "2018_19", field: "2018_19", sortable: true },
+    { headerName: "2019_20", field: "2019_20", sortable: true },
+    { headerName: "2020_21_TRE", field: "2020_21_TRE", sortable: true },
+    { headerName: "2021_22_SRE", field: "2021_22_SRE", sortable: true },
+    { headerName: "2022_23_FRE", field: "2022_23_FRE", sortable: true },
+    { headerName: "2023_24_PE", field: "2023_24_PE", sortable: true },
   ]);
 
   const handleLogout = () => {
     // Clear the localStorage and navigate to the login page or home page
-    localStorage.removeItem('role');
-    localStorage.removeItem('Username');
-    router.push('/'); // Or replace '/login' with the route you want to navigate to
+    localStorage.removeItem("role");
+    localStorage.removeItem("Username");
+    router.push("/"); // Or replace '/login' with the route you want to navigate to
   };
   return (
-
     <>
-    
-<div className="col-xl-12 text-end mt-3">
-                <button
-                  type="button"
-                  className="btn btn-danger"
-                  onClick={handleLogout}
-                >
-                  Logout
-                </button>
-              </div>
-
-              
-    <div style={{padding:"0px 21px"}}>
-      <div className="ag-theme-alpine" style={{ height: "100vh", width: "100%" }}>
-        <AgGridReact
-          rowData={rowData}
-          columnDefs={columnDefs}
-          modules={[ClientSideRowModelModule, ValidationModule]} // Include ValidationModule here
-        />
+      <div className="col-xl-12 d-flex justify-content-between align-items-center  py-4 px-4">
+        <div className="d-flex gap-2 align-items-center">
+          <img className="DashboardIcon" style={{height:"40px",width:"40px"}} src="/dashboardicon.png" alt=""/>
+          <h4 className="font-semibold text-black">Dashboard</h4>
+        </div>
+        <button type="button" className="btn btn-danger" onClick={handleLogout}>
+          Logout
+        </button>
       </div>
-    </div>
+
+      <div style={{ padding: "0px 21px" }}>
+        <div
+          className="ag-theme-alpine"
+          style={{ height: "100vh", width: "100%" }}
+        >
+          <AgGridReact
+            rowData={rowData}
+            columnDefs={columnDefs}
+            modules={[ClientSideRowModelModule, ValidationModule]} // Include ValidationModule here
+          />
+        </div>
+      </div>
     </>
   );
 }
